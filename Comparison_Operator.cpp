3e1da4cb95848e176cba1a8f65fe281c431ef7b4
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-30 13:06:15 +0530

    Add files via upload

---
+++ Comparison_Operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=5;
+	int b=6;
+	
+	cout<<"The value of a==b is: "<<(a==b)<<endl;
+	cout<<"The value of a<=b is: "<<(a<=b)<<endl;
+	cout<<"The value of a>=b is: "<<(a>=b)<<endl;
+	cout<<"The value of a<b is: "<<(a<b)<<endl;
+	cout<<"The value of a>b is: "<<(a>b)<<endl;
+	cout<<"The value of a!=b is: "<<(a!=b)<<endl;
+
+	return 0;
+}
