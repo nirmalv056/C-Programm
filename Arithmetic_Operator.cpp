4bbc1ddfd467e538f77a6a80eb3b0a50ef184279
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-30 13:05:42 +0530

    Add files via upload

---
+++ Arithmetic_Operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a=5;
+	int b=6;
+	
+	cout<<"The value of a+b is: "<<a+b<<endl;
+	cout<<"The value of a-b is: "<<a-b<<endl;
+	cout<<"The value of a*b is: "<<a*b<<endl;
+	cout<<"The value of a/b is: "<<a/b<<endl;
+	cout<<"The value of a%b is: "<<a%b<<endl;
+	cout<<"The value of a++ is: "<<a++<<endl;
+	cout<<"The value of a-- is: "<<a--<<endl;
+	cout<<"The value of ++a is: "<<++a<<endl;
+	cout<<"The value of --a is: "<<--a<<endl;
+
+
+	return 0;
+}
