484bf64aacea0915899625e26bcc7cf80ddea19a
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-30 13:05:59 +0530

    Add files via upload

---
+++ Assignment_Operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){	
+	int a = 10; 
+    cout << "Value of a is "<<a<<"\n"; 
+  
+    a += 10; 
+    cout << "Value of a+= is "<<a<<"\n"; 
+  
+    a -= 10; 
+    cout << "Value of a-= is "<<a<<"\n"; 
+  
+    a *= 10; 
+    cout << "Value of a*= is "<<a<<"\n"; 
+  
+    a /= 10; 
+    cout << "Value of a/= is "<<a<<"\n"; 
+}
