87ea3ec58c6f045e2ad827b27b3edd501783dbca
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-31 21:04:25 +0530

    Add files via upload

---
+++ Call_By_ Reference.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void swapPointer(int* a, int* b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+int main(){
+	int x=4, y=5;
+	
+	swapPointer(&x, &y);
+	cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+	
+	return 0;
+}
